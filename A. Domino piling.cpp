d8152f69e5ec2d3db739e3401e646f8bdb935e35
Mohannad Abdallah <151975004+Mohannadx101@users.noreply.github.com>
2024-04-12 08:56:21 +0200

    Create A. Domino piling.cpp
    You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
    
    1. Each domino completely covers two squares.
    
    2. No two dominoes overlap.
    
    3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
    
    Find the maximum number of dominoes, which can be placed under these restrictions.
    
    Input
    In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
    
    Output
    Output one number — the maximal number of dominoes, which can be placed.

---
+++ A. Domino piling.cpp
@@ -0,0 +1,17 @@
+// Link: https://codeforces.com/problemset/problem/50/A
+// 30ms / 0kb 
+
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int M, N;
+    
+    cin >> M >> N;
+    
+    cout << (M * N) / 2 << endl;
+    return 0;
+}
